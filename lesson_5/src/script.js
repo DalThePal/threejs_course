30c53c87ca1d9dbfc87f2639bbbe9941852a55b9
Dallen Hoyal <dallen.hoyal@gmail.com>
2021-04-26 20:23:07 -0600

    finished lesson 5

--- lesson_5/src/script.js
+++ lesson_5/src/script.js
@@ -1,40 +1,82 @@
 import './style.css'
 import * as THREE from 'three'
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+console.log(THREE)
 
-// Scene
+// SCENE
 const scene = new THREE.Scene()
 
-/**
- * Objects
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.set(0, 1, 0)
+scene.add(group)
 
-/**
- * Sizes
- */
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "yellow" })
+)
+
+group.add(cube1)
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "green" })
+)
+cube2.position.set(-2, 0, 0)
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "orange" })
+)
+cube3.position.set(2, 0, 0)
+group.add(cube3)
+
+
+// // MESH (cube)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({
+//   color: 'red'
+// })
+// const mesh = new THREE.Mesh(geometry, material)
+
+// // position
+// // mesh.position.x = 3
+// // mesh.position.y = 2
+// // mesh.position.z = 1
+// mesh.position.set(0.7, - 0.6, 1)
+
+// // scale
+// // mesh.scale.x = 2
+// // mesh.scale.y = 0.5
+// // mesh.scale.z = 0.5
+// mesh.scale.set(2, 0.5, 0.5)
+
+// // rotation
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
+// scene.add(mesh)
+
+// // AXES HELPER
+// const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper)
+
+// CAMERA
 const sizes = {
-    width: 800,
-    height: 600
+  width: 800,
+  height: 600
 }
+const ratio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(75, ratio)
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+
 scene.add(camera)
 
-/**
- * Renderer
- */
+// RENDERER
+let canvas = document.getElementById('canvas')
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+renderer.render(scene, camera) 
