872f7429bb64c79f394c0fa59da31bf0684c20bc
Dallen Hoyal <dallen.hoyal@gmail.com>
2021-06-22 11:10:39 -0600

    started lesson 17

---
+++ lesson_17_particles/src/script.js
@@ -0,0 +1,132 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+
+const particleTextures = {
+  1: textureLoader.load('/textures/particles/1.png'),
+  2: textureLoader.load('/textures/particles/2.png'),
+  3: textureLoader.load('/textures/particles/3.png'),
+  4: textureLoader.load('/textures/particles/4.png'),
+  5: textureLoader.load('/textures/particles/5.png'),
+  6: textureLoader.load('/textures/particles/6.png'),
+  7: textureLoader.load('/textures/particles/7.png'),
+  8: textureLoader.load('/textures/particles/8.png'),
+  9: textureLoader.load('/textures/particles/9.png'),
+  10: textureLoader.load('/textures/particles/10.png'),
+  11: textureLoader.load('/textures/particles/11.png'),
+  12: textureLoader.load('/textures/particles/12.png'),
+  13: textureLoader.load('/textures/particles/13.png'),
+}
+
+// particles
+const particleData = {
+  radius: 1,
+  widthSegments: 32,
+  heightSegments: 32,
+  size: 0.02,
+  sizeAttenuation: true,
+  numParticles: 500
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+
+const positions = new Float32Array(particleData.numParticles * 3)
+
+for (let i = 0; i < particleData.numParticles * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10
+}
+
+const particlePositions = new THREE.BufferAttribute(positions, 3)
+particlesGeometry.setAttribute('position', particlePositions)
+
+
+const particlesMaterial = new THREE.PointsMaterial({
+  size: particleData.size,
+  sizeAttenuation: particleData.sizeAttenuation
+})
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
